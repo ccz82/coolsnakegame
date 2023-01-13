0485db0dca36fdb14bf99ffb564a11209e85a308
ccz82 <chunzhen82@gmail.com>
2023-01-13 15:24:54 +0800

    Added a README.md and reworked gameInstance class into a Snake class

---
+++ README.md
@@ -0,0 +1,57 @@
+
+# cool snake game
+
+just a cool snake game
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+## How to clone repository locally
+
+Clone the project repository
+
+```bash
+  git clone https://github.com/ccz82/coolsnakegame.git
+```
+
+Go to the project directory
+
+```bash
+  cd coolsnakegame
+```
+
+Make sure pygame is installed
+
+```bash
+  pip install pygame
+```
+
+Start editing snake.py or to play it, run:
+
+```bash
+  python snake.py
+```
+## How to update repository locally
+
+Update the project repository
+
+```bash
+  git pull
+```
+## How to push changes to the GitHub repository
+
+Add all untracked files into the local git repository
+
+```bash
+  git add --all
+```
+
+Commit the changes
+
+```bash
+  git commit -m "Your message"
+```
+
+Push the changes
+
+```bash
+  git push
+```
--- snake.py
+++ snake.py
@@ -5,19 +5,11 @@ pygame.init()
 # Import randint function
 from random import randint
 
-# Draw a new window and return Surface object
-def initWindow(caption = "cool snake game", width = 512, height = 512):
-    # Set window title
-    pygame.display.set_caption(caption)
-    # Set window size and return Surface object
-    return pygame.display.set_mode((width, height))
-
-class gameInstance:
+class Snake:
 
     def __init__(
                 self,
                 gameSurface,
-                gameColor = (255, 255, 255),
                 snakeColor = (255, 0, 0),
                 snakePosX = 256,
                 snakePosY = 256,
@@ -26,14 +18,12 @@ class gameInstance:
                 snakeDirection = ''
                 ):
         self.gameSurface = gameSurface
-        self.gameColor = gameColor
         self.snakeColor = snakeColor
         self.snakePosX = snakePosX
         self.snakePosY = snakePosY
         self.snakeSizeX = snakeSizeX
         self.snakeSizeY = snakeSizeY
         self.snakeDirection = snakeDirection
-        self.gameLoop()
 
     def drawHead(self):
         rect = pygame.Rect(
@@ -55,56 +45,58 @@ class gameInstance:
         if self.snakeDirection == 'D':
             self.snakePosY += 16
 
-    def gameLoop(self):
-
-        # Game loop
-        exit = False
-        while not exit:
-
-            # Check if the user has exited the game
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit = True
-
-            # Draw snake head
-            self.drawHead()
-
-            # Get direction
-            key = pygame.key.get_pressed()
-            if key[pygame.K_LEFT] or key[pygame.K_a]:
-                self.snakeDirection = 'L'
-            elif key[pygame.K_RIGHT] or key[pygame.K_d]:
-                self.snakeDirection = 'R'
-            elif key[pygame.K_UP] or key[pygame.K_w]:
-                self.snakeDirection = 'U'
-            elif key[pygame.K_DOWN] or key[pygame.K_s]:
-                self.snakeDirection = 'D'
-
-            # Update direction
-            self.updateDirection()
-
-            # Clear board
-            self.gameSurface.fill(self.gameColor)
-
-            # Draw snake head
-            self.drawHead()
-
-            # Time delay in ms, lower delay = higher snake speed
-            pygame.time.wait(120)
-
+# Main program
 if __name__ == "__main__":
-    surface = initWindow()
-    gameInstance(surface)
 
-# Update local git repository
-"""
-git pull
-"""
+    # Variable assignment
+    caption = "cool snake game"
+    width = 512
+    height = 512
+    white = pygame.Color(255, 255, 255)
 
-# Commit changes to local git repository and push to GitHub
-"""
-git add --all
-git commit -m "message"
-git push
-"""
+    # Set window title
+    pygame.display.set_caption(caption)
 
+    # Set window size and get pygame.Surface
+    surface = pygame.display.set_mode((width, height))
+
+    # Set FPS counter
+    fps = pygame.time.Clock()
+
+    # Create a Snake object, passing in pygame.Surface
+    snake = Snake(gameSurface = surface)
+
+    # Game loop
+    exit = False
+    while not exit:
+
+        # Check if the user has exited the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit = True
+
+        # Draw snake head
+        snake.drawHead()
+
+        # Get direction
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            snake.snakeDirection = 'L'
+        elif key[pygame.K_RIGHT] or key[pygame.K_d]:
+            snake.snakeDirection = 'R'
+        elif key[pygame.K_UP] or key[pygame.K_w]:
+            snake.snakeDirection = 'U'
+        elif key[pygame.K_DOWN] or key[pygame.K_s]:
+            snake.snakeDirection = 'D'
+
+        # Update direction
+        snake.updateDirection()
+
+        # Clear board
+        snake.gameSurface.fill(white)
+
+        # Draw snake head
+        snake.drawHead()
+
+        # FPS management
+        fps.tick(10)
